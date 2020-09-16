3e27ccd894aa8a241a159e9cb867bb8cd9ccc50b
Danijel Gitanovic <dani025a@edu.ucl.dk>
2020-09-16 14:23:08 +0200

    Ændret

--- src/Moms.java
+++ src/Moms.java
@@ -11,7 +11,7 @@ public class Moms {
         double plusDanskMoms = købeSum * 0.25;
         double plusTyskMoms = købeSum * 0.16;
         
-        System.out.println("købs momsen er i DDK " + plusDanskMoms);
-        System.out.println("Den tyske moms i DKK " + (plusTyskMoms * 100)/ 100);
+        System.out.println("købs momsen er i DDK " + (int) (plusDanskMoms * 100)/ 100.0);
+        System.out.println("Den tyske moms i DKK " + (int) (plusTyskMoms * 100)/ 100.0);
     }
 }
