40f4908cfcb9de4f7e8fadc2f76a8bf5e049db25
Danijel Gitanovic <dani025a@edu.ucl.dk>
2020-09-16 15:24:07 +0200

    Færdig

--- src/Moms.java
+++ src/Moms.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Moms {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
         System.out.print("Skriv købe summen: ");
+        plusDanskMoms();
+        plusTyskMoms();
+    }
+    public static void plusDanskMoms(){
+        Scanner input = new Scanner(System.in);
         double købeSum = input.nextDouble();
-
-
         double plusDanskMoms = købeSum * 0.25;
-        double plusTyskMoms = købeSum * 0.16;
-        
+        double tyskMoms = købeSum * 0.16;
+
         System.out.println("købs momsen er i DDK " + (int) (plusDanskMoms * 100)/ 100.0);
-        System.out.println("Den tyske moms i DKK " + (int) (plusTyskMoms * 100)/ 100.0);
+        System.out.println("Den tyske moms i DKK " + (int) (tyskMoms * 100)/ 100.0);
+    }
+    public static void plusTyskMoms(){
+        Scanner input = new Scanner(System.in);
+        double købeSum = input.nextDouble();
+        double tyskMoms = købeSum * 0.16;
+        System.out.println("Den tyske moms i DKK " + (int) (tyskMoms * 100)/ 100.0);
     }
 }
