abae5a71cd9e27b913fa9e53e3481e25517d1f44
Danijel <70882611+Dani025a@users.noreply.github.com>
2020-09-16 16:02:33 +0200

    Update Moms.java

--- src/Moms.java
+++ src/Moms.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 
 public class Moms {
     public static void main(String[] args) {
-
-        System.out.print("Skriv købe summen: ");
         Scanner input = new Scanner(System.in);
+        System.out.print("Skriv købe summen: ");
         double købeSum = input.nextDouble();
+        
         plusDanskMoms(købeSum);
         plusTyskMoms(købeSum);
-
     }
-
     public static void plusDanskMoms(double købeSum){
         double danskMoms = købeSum * 0.25;
 
         System.out.println("købs momsen er i DDK " + (int) (danskMoms * 100)/ 100.0);
     }
     public static void plusTyskMoms(double købeSum){
-
         double plusTyskMoms = købeSum * 0.16;
+        
         System.out.println("Den tyske moms i DKK " + (int) (plusTyskMoms * 100)/ 100.0);
     }
 
