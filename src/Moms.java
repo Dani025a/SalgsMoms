82344f1de70b5571ca38ee3536ea846e74d57910
Danijel Gitanovic <dani025a@edu.ucl.dk>
2020-09-16 15:52:56 +0200

    Det virker nu!

--- src/Moms.java
+++ src/Moms.java
@@ -4,17 +4,23 @@ public class Moms {
     public static void main(String[] args) {
 
         System.out.print("Skriv købe summen: ");
-        plusDanskMoms();
-    }
-    public static void plusDanskMoms(){
         Scanner input = new Scanner(System.in);
         double købeSum = input.nextDouble();
+        plusDanskMoms(købeSum);
+        plusTyskMoms(købeSum);
 
-        double plusDanskMoms = købeSum * 0.25;
-        double tyskMoms = købeSum * 0.16;
-
-        System.out.println("købs momsen er i DDK " + (int) (plusDanskMoms * 100)/ 100.0);
-        System.out.println("Den tyske moms i DKK " + (int) (tyskMoms * 100)/ 100.0);
     }
+
+    public static void plusDanskMoms(double købeSum){
+        double danskMoms = købeSum * 0.25;
+
+        System.out.println("købs momsen er i DDK " + (int) (danskMoms * 100)/ 100.0);
+    }
+    public static void plusTyskMoms(double købeSum){
+
+        double plusTyskMoms = købeSum * 0.16;
+        System.out.println("Den tyske moms i DKK " + (int) (plusTyskMoms * 100)/ 100.0);
+    }
+
 
 }
